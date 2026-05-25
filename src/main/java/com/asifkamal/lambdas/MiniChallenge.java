1fe98b6c95b86d5e8feb166120443c7211b6c85f
asif-kamal <asif.kamal1489@gmail.com>
2026-05-25 18:00:22 -0400

    using unaryOperator to accept and return a String.

--- src/main/java/com/asifkamal/lambdas/MiniChallenge.java
+++ src/main/java/com/asifkamal/lambdas/MiniChallenge.java
@@ -4,11 +4,24 @@ import org.w3c.dom.ls.LSOutput;
 
 import java.util.Arrays;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 public class MiniChallenge {
 
     static Consumer<String> printTheParts = s ->
         Arrays.stream(s.split(" ")).forEach(System.out::println);
+
+    UnaryOperator<String> everySecondChar = source -> {
+        StringBuilder sb = new StringBuilder();
+        for  (int i = 0; i < source.length(); i++) {
+            if (i % 2 == 1) {
+                sb.append(source.charAt(i));
+            }
+        }
+        return sb.toString();
+
+    };
 
     public static void main(String[] args) {
         printTheParts.accept("This is a mini challenge");
