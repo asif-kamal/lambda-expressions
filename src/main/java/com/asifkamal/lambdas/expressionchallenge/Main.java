b907603282daf04eae239e1b5e357545871da1ba
asif-kamal <asif.kamal1489@gmail.com>
2026-05-30 13:32:18 -0500

    set all names to uppercase

---
+++ src/main/java/com/asifkamal/lambdas/expressionchallenge/Main.java
@@ -0,0 +1,19 @@
+package com.asifkamal.lambdas.expressionchallenge;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+
+    private static Random random = new Random();
+
+    public static void main(String[] args) {
+
+        String[] names = {"Anna", "Bob", "Carole", "David", "Ed", "Fred", "Gary"};
+
+        Arrays.setAll(names, i -> names[i].toUpperCase());
+        System.out.println("--> Transform to Uppercase");
+        System.out.println(Arrays.toString(names));
+    }
+}
